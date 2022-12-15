c9ef3e657a68285b0ed3941859ffec27d4e1fb81
Den <52295780+josserden@users.noreply.github.com>
2022-12-15 16:52:57 +0200

    add config for inline svg

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-const { i18n } = require('./next-i18next.config')
+const { i18n } = require('./next-i18next.config');
 
 module.exports = {
   i18n,
-}
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
